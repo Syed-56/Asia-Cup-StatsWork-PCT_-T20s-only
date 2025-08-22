cca5dddeb38994120c2f6845500b27579fe25e79
Syed-56 <ssa.56.sultan@gmail.com>
2025-08-22 20:24:54 +0500

    Scraped Data

---
+++ data-scraping.py
@@ -0,0 +1,15 @@
+import kagglehub
+import pandas as pd
+path = kagglehub.dataset_download("hasibalmuzdadid/asia-cup-cricket-1984-to-2022")
+
+matches = pd.read_csv(f"{path}/asiacup.csv")
+champions = pd.read_csv(f"{path}/champion.csv")
+batsmanDataT20 = pd.read_csv(f"{path}/batsman data t20i.csv")
+bowlerDataT20 = pd.read_csv(f"{path}/bowler data t20i.csv")
+wkpDataT20 = pd.read_csv(f"{path}/wicketkeeper data t20i.csv")
+
+matches.reset_index(drop=True, inplace=True)
+champions.reset_index(drop=True, inplace=True)
+batsmanDataT20.reset_index(drop=True, inplace=True)
+bowlerDataT20.reset_index(drop=True, inplace=True)
+wkpDataT20.reset_index(drop=True, inplace=True)
